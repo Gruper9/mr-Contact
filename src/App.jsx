4f37807afa666dcd3476c4a932a7423b9ebb8238
NaveDavid19 <navedavid16@gmail.com>
2023-12-31 18:23:11 +0200

    Add ContantIndex to rootcmp

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import './assets/style/main.css'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
+import { Provider } from "react-redux"
+import "./assets/style/main.css"
 
-
-import { HomePage } from './pages/HomePage'
-import { AboutUs } from './pages/AboutUs'
-import { store } from './store/store'
+import { HomePage } from "./pages/HomePage"
+import { AboutUs } from "./pages/AboutUs"
+import { store } from "./store/store"
+import { ContactIndex } from "./pages/ContactIndex"
 
 export function App() {
-
   return (
     <Provider store={store}>
       <Router>
@@ -17,7 +16,7 @@ export function App() {
             <Routes>
               <Route element={<HomePage />} path="/" />
               <Route element={<AboutUs />} path="/about" />
-
+              <Route element={<ContactIndex />} path="/contact"></Route>
             </Routes>
           </main>
         </section>
@@ -25,5 +24,3 @@ export function App() {
     </Provider>
   )
 }
-
-
